877c57b6c4e3b92ddaa77cc8735a9ebbca80c10e
alexandre.brba <alexandre.brba@gmail.com>
2019-05-20 15:43:50 +0200

    Permet de récupérer en php la liste des photos sauvegardés sous forme base64

--- php/uploadImage.php
+++ php/uploadImage.php
@@ -4,22 +4,28 @@ $datadirectory= 'C://wamp64/www/Projet-PPD/data/';
 
 $dirNam = scandir($datadirectory , 1 );
 foreach ($dirNam as $i ) {
-	
-	$directory = 'C://wamp64/www/Projet-PPD/data/'.$i;
-	echo($directory);
-	if ( ! is_dir($directory)) {
-		exit('Invalid diretory path');
-	}
+	if ('.' !== $i  && '..' !== $i ){
+		$directory = 'C://wamp64/www/Projet-PPD/data/'.$i;
 
-	$files = array();
-	foreach(scandir($directory) as $file){
-		if (!'.' === $file  && !'..' === $file ){
-		$files[] = $file;
+		if ( ! is_dir($directory)) {
+			exit('Invalid diretory path');
 		}
-	}
-	
-	foreach($files as $file){
-		echo($file);
+		
+		$files = array();
+		foreach(scandir($directory) as $file){
+			if ('.' !== $file  && '..' !== $file ){
+				$files[$i]["name"] = $file;
+				
+				$path = $directory.'/'.$file;
+				$type = pathinfo($path, PATHINFO_EXTENSION);
+				$data = file_get_contents($path);
+				$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+				
+				$files[$i]["base64"] = $base64;
+			}
+		}
+		
+		print_r($files);
 	}
 }
 
